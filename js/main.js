eeb0f76f3d2572148c87cbff984d8fdb073d2463
Abdullahznuwn <118454342+Abdullahznuwn@users.noreply.github.com>
2022-11-17 17:18:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let name = document.getElementById("s")
+let email= document.getElementById("o")
+
+let btn = document.getElementById("btn_4")
+btn.onclick = function () {
+alert("Name Is : " + s.value + "\n" + "Email Is: " + o.value)
+}
